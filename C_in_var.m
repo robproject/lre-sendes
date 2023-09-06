37e6844b3b2bac0358a6b9fa8155f9b53d5b0375
Moral <Moral@10.39.243.185>
2023-09-06 14:45:50 -0700

    Edited comments

--- C_in_var.m
+++ C_in_var.m
@@ -1,4 +1,4 @@
-% Uncertainties in the variables using syms
+% Coefficient in Uncertainties of variables
 
 syms Cd D2 D1 Di B rho v xf xi q cP
 
@@ -11,9 +11,15 @@ D2_sensitivity = (diff(Cd,D2)/Cd*D2)^2
 B_sensitivity = (diff(Cd,B)/Cd*B)^2
 c_P_sensitivity = (diff(Cd,cP)/Cd*cP)^2
 rho_sensitivity = (diff(Cd,rho)/Cd*rho)^2
+%--------------------------Beta -----------------------------
+B = (D2^2)/(D1^2);
+B_sensitivity = (4*B^8)/(B^4 - 1)^2
+D1_sensitivity = (diff(B_sensitivity,D1)/B*D1)
+D2_sensitivity = (diff(B_sensitivity,D2)/B*D2)
+
 %----------------------------------------------------
 % Where q = v * (Area of the vessel)
-Cd = sqrt(rho/2)*Di^2*v*D2^(-2)*sqrt(1-(B)^4)*(cP)^(-1/2);
+Cd = sqrt(rho/2)*Di^2*v*D1^(-2)*sqrt(1-(B)^4)*(cP)^(-1/2);
 Di_sensitivity = (diff(Cd,Di)/Cd*Di)^2
 v_sensitivity = (diff(Cd,v)/Cd*v)^2
 %----------------------------------------------------
@@ -25,3 +31,17 @@ xf_sensitivity = (diff(Cd,xf)/Cd*xf)^2;
 xi_sensitivity = (diff(Cd,xi)/Cd*xi)^2;
 simplify(xf_sensitivity)
 simplify(xi_sensitivity)
+%%
+%% Volume Approach
+syms Cd D2 D1 Di B rho v xf xi q cP tf ti
+
+% Without decomposed dvolume
+% vars = [D2, D1, Di, rho, v,  cP];
+% Cd = 4(Di/2)^2 v * D2^(-2) * (2* cP * (rho * (1-(D2/D1)^4))^(-1))^(-1/2);
+
+% With decomposed dvolume
+vars = [D2, D1, Di, rho, cP, xf, xi, tf, ti];
+ Cd = 4*(Di/2)^2 *(xf-xi)/(tf-ti) * D2^(-2) * (2* cP * (rho * (1-(D2/D1)^4))^(-1))^(-1/2);
+
+get_elasticities('Cd', Cd, vars)
+
