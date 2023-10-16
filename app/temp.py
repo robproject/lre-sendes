1ff2811734a15253ca2304f178cfd2ab2a6a1c8f
Robert Duncan <robirtduncan@gmail.com>
2023-10-15 23:25:25 -0700

    feat: app with db testing

--- app/temp.py
+++
@@ -1,7 +0,0 @@
-from log import test
-
-for s in ['comp', 'loop', 'chunk']:
-    times = []
-    for i in range(5):
-        times.append(test(s))
-    print(f"Time for {s}: {sum(times)/len(times)}")
---
+++ sendes/calc.py
@@ -0,0 +1,4 @@
+with open('app/test.csv', 'r') as f:
+    content = f.readlines()
+    print(content)
+    
